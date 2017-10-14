4584a9e109d159b257f9fdcd750e9a98ad3e7d5e
krishna <krishna.01718@gmail.com>
2017-10-14 12:29:43 -0400

    Created method for pwd

--- src/com/test/LoginPage.java
+++ src/com/test/LoginPage.java
@@ -1,9 +1,15 @@
 package com.test;
 
 public class LoginPage {
-	
+
 	public void getUserName() {
 		System.out.println("krishan");
+
+	}
+
+	public void password() {
+		System.out.println("krishan.143");
+
 	}
 
 }
